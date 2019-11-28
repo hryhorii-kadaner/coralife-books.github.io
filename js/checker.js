ac65167a70a631b23501113c62057b3164ad243c
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-11-28 16:18:50 +0200

    Первое подключение

---
+++ js/checker.js
@@ -0,0 +1,20 @@
+function checkParams() {
+	var name = $('#name').val();
+	var email = $('#email').val();
+	var text = $('#text').val();
+
+	if(email.indexOf('@') != -1 && name.length != 0 && text.length != 0) {
+		$('#submit').removeAttr('disabled');
+	} else {
+		$('#submit').attr('disabled', 'disabled');
+	}
+}
+checkParams();
+
+function hideshow() {
+	document.getElementById("mG61Hd").style.cssText="animation:hierrr 1s 1 linear;display:none;";
+	document.getElementById("shower").style.cssText="display:block;animation:showwww 1s 1 linear;";
+}
+$('#subm').click(function remover(){
+	$('.privat').removeAttr('disabled');
+});
