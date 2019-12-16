89d221df970d60bc5e5af61281adf6b65d2ee64a
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-12-16 17:57:13 +0200

    Обновление

--- js/checker.js
+++ js/checker.js
@@ -19,6 +19,38 @@ $('#subm').click(function remover(){
 	$('.privat').removeAttr('disabled');
 });
 
+function checkParamsNext() {
+	var nameNext = $('#name_next').val();
+	var emailNext = $('#email_next').val();
+	var textNext = $('#text_next').val();
+
+	if(emailNext.indexOf('@') != -1 && nameNext.length != 0 && textNext.length != 0) {
+		$('#submit2').removeAttr('disabled');
+	} else {
+		$('#submit2').attr('disabled', 'disabled');
+	}
+}
+checkParamsNext();
+
+function hideshowNext() {
+	document.getElementById("formHider2").style.cssText="animation:hierrr 1s 1 linear;display:none;";
+	document.getElementById("shower2").style.cssText="display:block;animation:showwww 1s 1 linear;";
+}
+function folderChecker() {
+	if ($("#profile-tab").hasClass("active")) {
+		$(".life-organizer").css("display", "none",);
+		$(".training").css("display", "");
+	} else {
+		$(".life-organizer").css("display", "");
+		$(".training").css("display", "none");
+	}
+
+}
+folderChecker();
+// $('#subm').click(function remover(){
+// 	$('.privat').removeAttr('disabled');
+// });
+
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar");
