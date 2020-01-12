b149464100d324416fa09f3d7760e65f1b7f9aa3
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-01-12 11:10:07 +0200

    Update date-checker.js

--- js/date-checker.js
+++ js/date-checker.js
@@ -1,17 +1,17 @@
 
-function unblocker() {
-	var today = new Date();
-	var unblock = new Date(2020, 0, 8, 10, 0, 0);
-	var timer = today > unblock;
+// function unblocker() {
+// 	var today = new Date();
+// 	var unblock = new Date(2020, 0, 8, 10, 0, 0);
+// 	var timer = today > unblock;
 	
-	if (timer == true) {
-		$('#bonus-tab').removeAttr('disabled');
-		$('#bonus-tab').removeClass('disabled');		
-	} else {
-		$('#bonus-tab').attr('disabled');
-		$('#bonus-tab').addClass('disabled');
-	}
-	return false;
-}
+// 	if (timer == true) {
+// 		$('#bonus-tab').removeAttr('disabled');
+// 		$('#bonus-tab').removeClass('disabled');		
+// 	} else {
+// 		$('#bonus-tab').attr('disabled');
+// 		$('#bonus-tab').addClass('disabled');
+// 	}
+// 	return false;
+// }
 
-unblocker();
+// unblocker();
