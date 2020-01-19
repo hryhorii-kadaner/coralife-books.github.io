586e0ced178570dc25c4f3d28c3b5d1bb3d70181
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-01-19 12:08:38 +0200

    Автообновление даты

--- js/date-checker.js
+++ js/date-checker.js
@@ -1,17 +1,2 @@
-
-// function unblocker() {
-// 	var today = new Date();
-// 	var unblock = new Date(2020, 0, 8, 10, 0, 0);
-// 	var timer = today > unblock;
-	
-// 	if (timer == true) {
-// 		$('#bonus-tab').removeAttr('disabled');
-// 		$('#bonus-tab').removeClass('disabled');		
-// 	} else {
-// 		$('#bonus-tab').attr('disabled');
-// 		$('#bonus-tab').addClass('disabled');
-// 	}
-// 	return false;
-// }
-
-// unblocker();
+var d = new Date();
+document.write(d.getFullYear());
